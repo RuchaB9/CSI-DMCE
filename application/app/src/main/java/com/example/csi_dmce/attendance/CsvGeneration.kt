2a7cde0cf8d199f71f65d666decc23410392b4c6
RuchaB9 <ruchabhidem@gmail.com>
2023-04-15 18:01:12 +0530

    Display Excel sheet

--- application/app/src/main/java/com/example/csi_dmce/attendance/CsvGeneration.kt
+++ application/app/src/main/java/com/example/csi_dmce/attendance/CsvGeneration.kt
@@ -1,6 +1,8 @@
 package com.example.csi_dmce.attendance
-import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.os.Environment
+import android.util.Log
 import android.view.Gravity
 import android.view.ViewGroup
 import android.widget.Button
@@ -8,17 +10,22 @@ import android.widget.EditText
 import android.widget.PopupWindow
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
+import androidx.core.content.FileProvider
 import com.example.csi_dmce.R
+import java.io.File
+import java.io.FileNotFoundException
 
 class CsvGeneration: AppCompatActivity() {
 
     val ctx = this
+    val excelFile = File(Environment.getExternalStorageDirectory(), "attendance.csv").absolutePath
 
-    private lateinit var attendanceCard : CardView
-    private lateinit var expenseCard : CardView
-    private lateinit var registrantCard : CardView
 
-    override fun onCreate(savedInstanceState: Bundle?){
+    private lateinit var attendanceCard: CardView
+    private lateinit var expenseCard: CardView
+    private lateinit var registrantCard: CardView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.csv_generation)
 
@@ -27,7 +34,11 @@ class CsvGeneration: AppCompatActivity() {
         registrantCard = findViewById(R.id.cardView2)
 
         val popupView = layoutInflater.inflate(R.layout.csv_popup, null)
-        val popupWindow = PopupWindow(popupView, ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        val popupWindow = PopupWindow(
+            popupView,
+            ViewGroup.LayoutParams.MATCH_PARENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
 
         popupWindow.isOutsideTouchable = true
         popupWindow.isFocusable = true
@@ -46,21 +57,46 @@ class CsvGeneration: AppCompatActivity() {
 
         }
 
-        expenseCard.setOnClickListener{
+        expenseCard.setOnClickListener {
             val expenseID = editText.text.toString()
             ExpensesCSV.writeExpensesData(ctx, expenseID)
         }
 
-        registrantCard.setOnClickListener{
+        registrantCard.setOnClickListener {
             popupWindow.showAtLocation(registrantCard, Gravity.CENTER, 0, 0)
 
 
             submitButton.setOnClickListener {
                 val eventForCsv = editText.text.toString()
                 RegistrantsCsv.writeRegistrantsData(ctx, eventForCsv)
+
+                openExcelSheet(excelFile)
+
+            }
+
+        }
+    }
+
+    private fun openExcelSheet(filePath:String) {
+        val file = File(filePath)
+        if (file.exists()) {
+            try {
+                val uri = FileProvider.getUriForFile(
+                    applicationContext,
+                    "applicationContext.com.example.csi_dmce.attendance.provider",
+                    file
+                )
+                val intent = Intent(Intent.ACTION_VIEW)
+                intent.setDataAndType(uri, "application/vnd.ms-excel")
+                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                if (intent.resolveActivity(applicationContext.packageManager) != null) {
+                    startActivity(intent)
+                }
+
+            } catch (e: FileNotFoundException){
+                Log.d("Missing file", "File doesnt exist")
+                throw e
             }
         }
-
-
     }
 }
